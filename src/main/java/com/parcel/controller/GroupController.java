26936c155755b8069b6a260fd9a578868908f8fc
rapa <rapa@rapa2>
2016-10-29 11:11:50 +0900

    a

--- src/main/java/com/parcel/controller/GroupController.java
+++ src/main/java/com/parcel/controller/GroupController.java
@@ -3,6 +3,7 @@ package com.parcel.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
